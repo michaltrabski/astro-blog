3edafc4899d9bda3a4db775fbea5a93c60ee9579
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2022-12-22 14:51:17 +0100

    3 files modified

--- src/reactComponents/Counter.jsx
+++ src/reactComponents/Counter.jsx
@@ -4,3 +4,8 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   return <div onClick={() => setCount((c) => c + 1)}>count = {count}</div>;
 }
+
+export const Year = () => {
+  const [date, setDate] = useState(Date.now());
+  return <span onClick={() => setDate((c) => c + 111111)}>{date}</span>;
+};
