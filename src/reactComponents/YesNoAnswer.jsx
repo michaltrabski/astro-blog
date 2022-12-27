639f79811b90706d8e6df3a74ddcd6692ca1513e
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2022-12-27 12:00:34 +0100

    1 file modified, 1 added

---
+++ src/reactComponents/YesNoAnswer.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const possibleAnswers = [
+  { value: 'YES', label: 'TAK' },
+  { value: 'NO', label: 'Nie' },
+];
+
+const correctAnswerValue = 'YES';
+
+export default function YesNoAnswer() {
+  const [clickedAnswerValue, setClickedAnswerValue] = useState(null);
+
+  return (
+    <div>
+      clickedAnswerValue === {clickedAnswerValue} <br />
+      {possibleAnswers.map((answer) => {
+        let btnColor = 'danger';
+
+        if (answer.value === correctAnswerValue) {
+          btnColor = 'success';
+        }
+
+        if (!clickedAnswerValue) {
+          btnColor = 'secondary';
+        }
+
+        return (
+          <>
+            <button
+              key={possibleAnswers.value}
+              onClick={() => setClickedAnswerValue(answer.value)}
+              type="button"
+              className={`btn btn-${btnColor} me-2`}
+            >
+              {answer.label}
+            </button>
+          </>
+        );
+      })}
+    </div>
+  );
+}
