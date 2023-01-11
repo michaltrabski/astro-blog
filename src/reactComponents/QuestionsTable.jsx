5676e65f3f9a93ce44c84a7e403868c6fa368e3d
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2023-01-11 14:57:14 +0100

    2 files modified, 1 added

---
+++ src/reactComponents/QuestionsTable.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { useStore } from '@nanostores/react';
+import { questions } from '../store/questions';
+
+// interface Props {
+//   x: string;
+// }
+
+export default function QuestionsTable(props) {
+  const $questions = useStore(questions);
+
+  return (
+    <div>
+      <h1>QuestionsTable:</h1>
+
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              {['#', 'text'].map((headerEl) => (
+                <th scope="col">{headerEl}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {$questions.map((q, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>
+                  <a href={`/${q.id}`}>{q.text}</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        {$questions.map((q, index) => (
+          <p key={q.id}>
+            {index + 1}. {q.text}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}
