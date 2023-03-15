2562a1b284bbb2c8afae5c61c8e8028a61619b06
Michal Trabski <michal.trabski@mobica.com>
2023-03-15 10:15:16 +0100

    load play button if mp3 canplay

--- src/reactComponents/Mp3Player.tsx
+++ src/reactComponents/Mp3Player.tsx
@@ -2,7 +2,14 @@ import React, { useRef, useEffect, useState, useMemo } from "react";
 import { useStore } from "@nanostores/react";
 import clsx from "clsx";
 
-import { _mp3Items, _addMp3Item, _playMp3Item, Mp3Item, _updateMp3ItemState, _updateMp3ItemCanPlay } from "../store/store";
+import {
+  _mp3Items,
+  _addMp3Item,
+  _playMp3Item,
+  Mp3Item,
+  _updateMp3ItemState,
+  _updateMp3ItemCanPlay,
+} from "../store/store";
 import { MEDIA_HOST, MP3_DIR, MP3_EXTENSION } from "../settings/settings";
 
 export default function Mp3Player() {
@@ -33,10 +40,9 @@ interface SingleMp3Props {
 }
 
 const SingleMp3 = (props: SingleMp3Props) => {
-  const { id, canplay,  state, action } = props.mp3Item;
+  const { id, canplay, state, action } = props.mp3Item;
 
   const mp3ItemsMap = useStore(_mp3Items);
- 
 
   const audioRef = useRef<HTMLAudioElement>(null);
 
@@ -52,24 +58,13 @@ const SingleMp3 = (props: SingleMp3Props) => {
   const pause = () => audioRef?.current?.pause();
 
   useEffect(() => {
-    if (mp3ItemsMap[id].action === "pause") {
-      // console.log("item to pause is", mp3ItemsMap[id]);
-      pause();
-    }
-
-    if (mp3ItemsMap[id].action === "play") {
-      // console.log("item to play is", mp3ItemsMap[id]);
-      play();
-    }
+    if (mp3ItemsMap[id].action === "pause") pause();
+    if (mp3ItemsMap[id].action === "play") play();
   }, [mp3ItemsMap]);
-
-
-
-
 
   const errorCallback = () => console.log("error");
   const canplayCallback = () => {
-     if (_mp3Items.get()[id].canplay === false) {
+    if (_mp3Items.get()[id].canplay === false) {
       _updateMp3ItemCanPlay(id, true);
     }
   };
@@ -77,12 +72,10 @@ const SingleMp3 = (props: SingleMp3Props) => {
   const pauseCallback = () => console.log("pause");
   const endedCallback = () => console.log("ended");
 
-
   // detect if file is playing
   useEffect(() => {
     const audio = audioRef.current;
     // console.log("id === ", id)
-
 
     if (audio) {
       audio.addEventListener("error", errorCallback);
@@ -109,14 +102,18 @@ const SingleMp3 = (props: SingleMp3Props) => {
 
       {/* <pre>{JSON.stringify(mp3ItemsArray,null,2)}</pre> */}
 
-      <div className="d-flex justify-content-between align-items-center">
+      <div>
         <audio ref={audioRef} controls src={MEDIA_HOST + MP3_DIR + id + MP3_EXTENSION} />
-        <i className="bi bi-play-circle" onClick={() => _playMp3Item(id)}></i>
-        <i className="bi bi-pause-circle"></i>
-        <i className="bi bi-stop-circle"></i>
-        <span>canplay = {JSON.stringify(canplay)}</span>
-        <span>state = {state}</span>
-        <span>action = {action}</span>
+        <div className="d-flex justify-content-between align-items-center">
+          <i className="bi bi-play-circle" onClick={() => _playMp3Item(id)}></i>
+          <i className="bi bi-pause-circle"></i>
+          <i className="bi bi-stop-circle"></i>
+        </div>
+        <div className="d-flex justify-content-between align-items-center">
+          <span>canplay = {JSON.stringify(canplay)}</span>
+          <span>state = {state}</span>
+          <span>action = {action}</span>
+        </div>
       </div>
     </div>
   );
--- src/reactComponents/QuestionText.tsx
+++ src/reactComponents/QuestionText.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect, useState } from "react";
 import { useStore } from "@nanostores/react";
 import clsx from "clsx";
 
-import { QuestionPageData, _playMp3Item } from "../store/store";
+import { QuestionPageData, _mp3Items, _playMp3Item } from "../store/store";
 import { _addMp3Item } from "../store/store";
 import slugify from "slugify";
 import Mp3 from "./Mp3";
@@ -13,29 +13,34 @@ interface QuestionTextProps {
 
 export default function QuestionText(props: QuestionTextProps) {
   const { text } = props.question;
- 
-    const slugText = slugify(text, {
-      replacement: "-", // replace spaces with replacement character, defaults to `-`
-      remove: /[*+~,.()/'"!:@?;]/g, // remove characters that match regex, defaults to `undefined`
-      lower: true, // convert to lower case, defaults to `false`
-      strict: false, // strip special characters except replacement, defaults to `false`
-      locale: "vi", // language code of the locale to use
-      trim: true, // trim leading and trailing replacement chars, defaults to `true`
-    });
 
-  // michal 
-  useEffect(() => {
-    _addMp3Item({ id: slugText  });
-  }, []);
+  useStore(_mp3Items); // calling this hook is needed to update the component when the store changes
+
+  const slugText = slugify(text, {
+    replacement: "-", // replace spaces with replacement character, defaults to `-`
+    remove: /[*+~,.()/'"!:@?;]/g, // remove characters that match regex, defaults to `undefined`
+    lower: true, // convert to lower case, defaults to `false`
+    strict: false, // strip special characters except replacement, defaults to `false`
+    locale: "pl", // language code of the locale to use
+    trim: true, // trim leading and trailing replacement chars, defaults to `true`
+  });
+
+   useEffect(() => _addMp3Item({ id: slugText }), []);
+
+   const canplay = _mp3Items.get()[slugText]?.canplay;
 
   return (
     <div className="row mb-3">
       <div className="col">
         <h1 className="display-6 text-start shadow-bottom">
           {text}
-          <button className="btn btn-light" onClick={() => _playMp3Item(slugText)}>
-            <span className="bi bi-play-circle"></span>
-          </button>
+
+          { canplay && (
+            <button className="btn btn-light" onClick={() => _playMp3Item(slugText)}>
+              <span className="bi bi-play-circle"></span>
+            </button>
+          )}
+         
         </h1>
       </div>
     </div>
