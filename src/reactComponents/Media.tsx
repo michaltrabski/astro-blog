accf882727d334f7964b3c5a33127c33e1d289be
Michal Trabski <michal.trabski@mobica.com>
2023-04-07 13:00:39 +0200

    layout change

--- src/reactComponents/Media.tsx
+++ src/reactComponents/Media.tsx
@@ -13,7 +13,10 @@ interface MediaProps {
 export default function Media(props: MediaProps) {
   const { text, media, showControls = false, startVideoAutomaticaly = true, size = "small" } = props;
 
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isEnded, setIsEnded] = useState(false);
+  const [hideControlsOnceWhenItStartsPlaying, setHideControlsOnceWhenItStartsPlaying] = useState(false);
+  const [showPlayIcon, setShowPlayIcon] = useState(false);
 
   const videoRef = React.useRef<HTMLVideoElement | null>(null);
   const pngRef = React.useRef<HTMLImageElement | null>(null);
@@ -30,73 +33,89 @@ export default function Media(props: MediaProps) {
   const mediaUrl = media === "placeholder.png" ? "/placeholder.png" : MEDIA_HOST + mediaSize + "" + media;
   const isVideo = media.endsWith(".mp4");
 
-  // useEffect(() => {
-  //   if (videoRef.current) {
-  //     videoRef.current.addEventListener("error", () => {
-  //       console.log("video error", mediaUrl);
-  //     });
-  //   }
+  function playCallback() {
+    console.log("video is playing", mediaUrl);
+    setIsPlaying(true);
+    setIsEnded(false);
+    setHideControlsOnceWhenItStartsPlaying(true);
+    setShowPlayIcon(false);
+  }
 
-  //   console.log("video", videoRef.current);
+  function pauseCallback() {
+    console.log("video is paused", mediaUrl);
+    setIsPlaying(false);
+    setShowPlayIcon(true);
+  }
 
-  //   if (pngRef.current) {
-  //     pngRef.current.addEventListener("error", () => {
-  //       console.log("png error", mediaUrl);
-  //     });
-  //   }
+  function endedCallback() {
+    console.log("video ended", mediaUrl);
+    setIsPlaying(false);
+    setIsEnded(true);
+    setHideControlsOnceWhenItStartsPlaying(true);
+    setShowPlayIcon(true);
+  }
 
-  //   console.log("png", pngRef.current);
-  // }, [videoRef.current, pngRef.current]);
+  useEffect(() => {
+    const video = videoRef.current;
+
+    video?.addEventListener("play", playCallback);
+    video?.addEventListener("pause", pauseCallback);
+    video?.addEventListener("ended", endedCallback);
+
+    return () => {
+      video?.removeEventListener("play", playCallback);
+      video?.removeEventListener("pause", pauseCallback);
+      video?.removeEventListener("ended", endedCallback);
+    };
+  }, []);
 
   const playVideo = () => {
-    if (videoRef.current) {
-      videoRef.current.currentTime = 0;
-      videoRef.current.play();
+    const video = videoRef.current;
+
+    if (video) {
+      video.currentTime = 0;
+      video.play();
     }
   };
 
   return (
     <div className="MEDIA row">
-      <div className="col">
-        <div className={`${isLoaded ? "border border-success": ""}`}>
-        {/* <p className="small position-absolute text-light">
-          {width}x{height}
+      <div className="col p-0">
+        {/* <p className="small position-absolute text-light bg-dark">
+          isPlaying: {isPlaying ? "true" : "false"} <br />
+          isEnded: {isEnded ? "true" : "false"} <br />
         </p> */}
-        {isVideo ? (
-          <>
-            <video
-              className="w-100 shadow border border-dark"
-              ref={videoRef}
+        {/* style={{ width: "calc(100% + 4rem)", transform: "translateX(-1rem)" }} */}
+        <div >
+        <div  style={{ overflow: "hidden",border: "0px solid red",scale: "1" }}>
+        <div  style={{ border: "0px solid green", scale: "1" }}>
+          {isVideo ? (
+            <div className="position-relative" onClick={() => playVideo()}>
+              <video
+                className="w-100 shadow border-dark"
+                ref={videoRef}
+                src={mediaUrl}
+                autoPlay={startVideoAutomaticaly}
+                controls={showControls && !hideControlsOnceWhenItStartsPlaying}
+              >
+                <p>{text || media}</p>
+              </video>
+              <div className={`position-absolute ${showPlayIcon || "d-none"}`} style={{ top: "50%", left: "50%" }}>
+                <div style={{ transform: "translate(-50%,-50%)" }}>
+                  <i style={{ fontSize: "2rem", cursor: "pointer" }} className="larger bi bi-play-circle"></i>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <img
+              className="w-100 shadow border border-dark img-fluid"
+              ref={pngRef}
               src={mediaUrl}
-              // autoPlay={import.meta.env.MODE === "development" ? false : stopAutoPlay ? false : true}
-              autoPlay={startVideoAutomaticaly}
-              controls={showControls}
-              onClick={() => {
-                console.log("click");
-                // playVideo();
-              }}
-              onLoad={() => {
-                console.log("Video loaded:", mediaUrl);
-                setIsLoaded(true);
-              }}
-            >
-              <p>{text || media}</p>
-            </video>
-          </>
-        ) : (
-          <img
-            className="w-100 shadow border border-dark img-fluid"
-            ref={pngRef}
-            src={mediaUrl}
-            alt={text || media}
-            onLoad={() => {
-              console.log("media loaded: ", mediaUrl);
-              setIsLoaded(true);
-            }}
-          />
-        )}
+              alt={text || media}
+            />
+          )}
+        </div>
       </div>
-      </div>
-    </div>
+    </div>  </div></div>
   );
 }
